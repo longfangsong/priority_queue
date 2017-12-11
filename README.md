0be70e168b66483fc5a8895a74c36f0113f56827
龙方淞 <longfangsong@icloud.com>
2017-12-11 23:21:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+**该项目尚未完工！**
+
+**This Project is in building!**
+# 优先队列
+
+这是一个任务管理App。
+
+作者开发这个App的原因是因为有选择障碍症，在手头有多个任务时往往不知道要先做什么好。
+
+希望能帮到和我有同样毛病的人……
+
+也希望有大佬来提意见&帮着开发。
+
+## 技术选型
+前端使用[quasar框架](http://quasar-framework.org)，这是一个用于构建网页和Hybrid App的框架。
+
+支持Vue全家桶和将前端页面用[Cordova](http://cordova.apache.org)封装成App。
+
+（曾经有一个未发布的版本使用的是Weex，然而Weex的文档太烂，对web标准的支持又太差，说是说用web技术构建原生App其实几乎完全是另外一套东西，限制特别大，于是只好弃了）。
+
+后端……暂时没有必要有后端。
+
+## 构建开发环境
+
+``` bash
+# 安装依赖
+$ npm install
+
+# 运行服务器（有hot reload） ，默认地址localhost:8080
+$ quasar dev
+
+# 构建production版本
+$ quasar build
+
+# lint code
+$ quasar lint
+
+# 在ios上运行
+$ ./runios.sh
+
+# 在Android上运行
+$ ./runandroid.sh
+
+# 运行功能测试（兼具Demo效果）
+$ python3 functional_test.py
+```
+---
+# Priority Queue
+
+This is a task management App.
+
+The reason the author developed this App is because of his disorder of choice, often having no idea what to do first when there are multiple tasks in hand.
+
+Hoping to help people who have the same problems as me.
+
+Also want to have some great guys to give some advice & help me in developing this.
+
+## Techs.
+The frontend uses [quasar-framework](http://quasar-framework.org),which is a framework to build Hybrid and Web Apps。
+
+Support Vue and its env. system, and [Cordova](http://cordova.apache.org) wrap it as an App。
+
+（I used to use Weex,but its document is terrible，and its terrible support on web standard is driving me mad, there're to many limits!）
+
+There's temporary no need to have a backend.
+
+## Build a dev env
+
+``` bash
+# install dependencys
+$ npm install
+
+# run a dev server with hot reload on localhost:8080
+$ quasar dev
+
+# build for production
+$ quasar build
+
+# lint code
+$ quasar lint
+
+# run on ios
+$ ./runios.sh
+
+# run on Android
+$ ./runandroid.sh
+
+# functional test(can also be regarded as a demo)
+$ python3 functional_test.py
+```
---
+++ functional_test.py
@@ -0,0 +1,106 @@
+import unittest
+from selenium.webdriver import Chrome
+from time import sleep
+# now I use chrome so this can be also a demo
+# if want it be a test only, consider using Phantom.js etc.
+
+class TestDefault(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.browser = Chrome()
+
+    def test_default(self):
+        # 用户打开了浏览器/App
+        self.browser.get('http://localhost:8080/#/')
+        sleep(1)
+        # 用户发现屏幕上有App的名称
+        self.assertIn('优先队列', self.browser.find_element_by_id('title').text)
+        # 用户发现屏幕上有“现在要做的事”，现在还不需要做什么事
+        self.assertIn('还没有要做的事……', self.browser.find_element_by_id('now-should-do').text)
+        # 用户发现屏幕底部可以用来切换页面
+        self.assertIn('目前任务', self.browser.find_elements_by_class_name('q-tab')[0].text)
+        self.assertIn('任务列表', self.browser.find_elements_by_class_name('q-tab')[1].text)
+
+        # 用户切换到了任务列表
+        self.browser.find_elements_by_class_name('q-tab')[1].click()
+        sleep(1)
+        # 用户发现任务列表空空如也
+        self.assertFalse(self.browser.find_elements_by_class_name('task'))
+
+        def add_task(self,task_name):
+            # 用户点击了左下角的图标
+            self.browser.find_element_by_id('functions-button').click()
+            sleep(1)
+            # 他点击了新建按钮
+            self.browser.find_element_by_id('new-item-button').click()
+            sleep(1)
+            # 有个对话框出现了，用户输入了这个任务的名字
+            self.browser.find_element_by_tag_name('input').send_keys(task_name)
+            # 用户点击了确定
+            self.browser.find_element_by_id('ok').click()
+            sleep(1)
+
+        # 用户决定添加一项任务
+        add_task('Task 1')
+        # 任务出现在了任务列表中
+        self.assertIn('Task 1', self.browser.find_element_by_class_name('task').text)
+        # 用户又添加了几项任务
+        add_task('Task 2')
+        self.assertIn('Task 2', self.browser.find_element_by_class_name('task').text)
+        add_task('Task 3')
+        self.assertIn('Task 3', self.browser.find_element_by_class_name('task').text)
+        add_task('Task 4')
+        self.assertIn('Task 3', self.browser.find_element_by_class_name('task').text)
+
+        # 用户回到了目前任务的页面
+        self.browser.find_elements_by_class_name('q-tab')[0].click()
+        sleep(1)
+        # 页面上现在有要做的事了
+        self.assertIn('Task 1', self.browser.find_element_by_id('now-should-do').text)
+
+        # 用户想要重新排列代办事项，他先进入了列表页
+        self.browser.find_elements_by_class_name('q-tab')[1].click()
+        sleep(1)
+        # 用户点击了左下角的图标
+        self.browser.find_element_by_id('functions-button').click()
+        sleep(1)
+        # 他点击了编辑按钮
+        self.browser.find_element_by_id('edit-order-button').click()
+        sleep(1)
+        # 他将Task2拖拽到了第一个，Task4拖拽到了第二个
+        # 此处需要手动干预测试……
+        # Action Chains don't currently work on Mac.（F**k）
+        sleep(30)
+        # 他回到了目前任务的页面
+        self.browser.find_elements_by_class_name('q-tab')[0].click()
+        sleep(1)
+        # 页面上要做的事更新了
+        self.assertIn('Task 2', self.browser.find_element_by_id('now-should-do').text)
+
+        # 用户已经完成Task 2了
+        self.browser.find_element_by_id('task-finish').click()
+        sleep(1)
+        # 现在目前要做的是Task 4
+        self.assertIn('Task 4', self.browser.find_element_by_id('now-should-do').text)
+
+        # 用户想要删除所有排列代办事项，他先进入了列表页
+        self.browser.find_elements_by_class_name('q-tab')[1].click()
+        sleep(1)
+        # 用户点击了左下角的图标
+        self.browser.find_element_by_id('functions-button').click()
+        sleep(1)
+        # 他点击了删除按钮
+        self.browser.find_element_by_id('delete-button').click()
+        sleep(1)
+        # 他点击了每个项目的删除按钮（应该有三个项目）
+        self.browser.find_element_by_class_name('delete-item').click()
+        sleep(1)
+        self.browser.find_element_by_class_name('delete-item').click()
+        sleep(1)
+        self.browser.find_element_by_class_name('delete-item').click()
+        sleep(1)
+        # 用户发现任务列表又空空如也了
+        self.assertFalse(self.browser.find_elements_by_class_name('task'))
+
+if __name__ == '__main__':
+    unittest.main()
